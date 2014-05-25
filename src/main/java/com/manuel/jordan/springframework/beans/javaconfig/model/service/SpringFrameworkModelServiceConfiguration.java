99b205c7fac6b969470d379149911178921f4897
Manuel Jordan Elera <dr_pompeii@yahoo.coom>
2014-05-25 14:10:39 -0500

    SpringFrameworkModelServiceConfiguration class edited

--- src/main/java/com/manuel/jordan/springframework/beans/javaconfig/model/service/SpringFrameworkModelServiceConfiguration.java
+++ src/main/java/com/manuel/jordan/springframework/beans/javaconfig/model/service/SpringFrameworkModelServiceConfiguration.java
@@ -1,5 +1,39 @@
 package com.manuel.jordan.springframework.beans.javaconfig.model.service;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.manuel.jordan.model.service.PersonService;
+import com.manuel.jordan.model.repository.PersonRepository;
+import com.manuel.jordan.model.service.impl.PersonServiceImplVersion01;
+import com.manuel.jordan.model.service.impl.PersonServiceImplVersion02;
+
+/**
+ * <p>Configuration class.
+ *    The purpose is declare beans how a Service.</p>
+ *   
+ * @author Manuel Jordan - dr_pompeii
+ *
+ */
+@Configuration
 public class SpringFrameworkModelServiceConfiguration {
 
+	@Bean
+	public PersonService personService01(PersonRepository personRepository){
+		
+		PersonService personService = new PersonServiceImplVersion01(personRepository);
+		
+		return personService;
+	}
+	
+	@Bean(name="personService02")
+	public PersonService personServiceImplVersion02(PersonRepository personRepository){
+		
+		PersonServiceImplVersion02 personService = new PersonServiceImplVersion02();
+		
+		personService.setPersonRepository(personRepository);
+		
+		return personService;
+	}
+	
 }
