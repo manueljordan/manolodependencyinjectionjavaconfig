0f01b4138e7b8789991ea92afefc30282b573a52
Manuel Jordan Elera <dr_pompeii@yahoo.coom>
2014-05-25 13:58:09 -0500

    SpringFrameworkModelRepositoryCustomConfiguration class edited

--- src/main/java/com/manuel/jordan/springframework/beans/javaconfig/model/repository/custom/SpringFrameworkModelRepositoryCustomConfiguration.java
+++ src/main/java/com/manuel/jordan/springframework/beans/javaconfig/model/repository/custom/SpringFrameworkModelRepositoryCustomConfiguration.java
@@ -1,5 +1,24 @@
 package com.manuel.jordan.springframework.beans.javaconfig.model.repository.custom;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.manuel.jordan.model.repository.PersonRepository;
+import com.manuel.jordan.model.repository.custom.PersonCustomRepository;
+
+/**
+ * <p>Configuration class.
+ *    The purpose is declare a bean how a Repository.</p>
+ *   
+ * @author Manuel Jordan - dr_pompeii
+ *
+ */
+@Configuration
 public class SpringFrameworkModelRepositoryCustomConfiguration {
 
+	@Bean
+	public PersonRepository personRepository(){
+		return new PersonCustomRepository();
+	}
+	
 }
