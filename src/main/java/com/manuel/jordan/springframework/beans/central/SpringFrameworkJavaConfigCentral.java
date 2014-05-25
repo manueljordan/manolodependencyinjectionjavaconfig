258d9c64ea1ad582acfc2bb6c7a12d470d7dd6ec
Manuel Jordan Elera <dr_pompeii@yahoo.coom>
2014-05-25 14:26:21 -0500

    SpringFrameworkJavaConfigCentral class edited

--- src/main/java/com/manuel/jordan/springframework/beans/central/SpringFrameworkJavaConfigCentral.java
+++ src/main/java/com/manuel/jordan/springframework/beans/central/SpringFrameworkJavaConfigCentral.java
@@ -1,5 +1,20 @@
 package com.manuel.jordan.springframework.beans.central;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.manuel.jordan.springframework.beans.javaconfig.model.service.SpringFrameworkModelServiceConfiguration;
+import com.manuel.jordan.springframework.beans.javaconfig.model.repository.custom.SpringFrameworkModelRepositoryCustomConfiguration;
+
+/**
+ * <p>Central Configuration class to be called from the Main class. 
+ *    The purpose is import other Configuration classes.</p>
+ *    
+ * @author Manuel Jordan - dr_pompeii
+ * 
+ */
+@Configuration
+@Import({SpringFrameworkModelRepositoryCustomConfiguration.class, SpringFrameworkModelServiceConfiguration.class})
 public class SpringFrameworkJavaConfigCentral {
 
 }
